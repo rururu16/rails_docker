6abf1287e80459b750925b3ba17a59087d64c625
rururu16 <ynk618172@gmail.com>
2018-12-13 21:27:46 +0900

    add link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # root 'boards#index'
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
-  get 'boards/:id', to: 'boards#show'
+  resources :boards, only: [:index, :new, :create, :show]
 end
