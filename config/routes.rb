08a9e8798d4ac4eccb792954354648cbcaa6aaf3
rururu16 <ynk618172@gmail.com>
2018-12-13 23:50:15 +0900

    add partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # root 'boards#index'
-  resources :boards, only: [:index, :new, :create, :show]
+  resources :boards, only: [:index, :new, :create, :show, :edit, :update]
 end
